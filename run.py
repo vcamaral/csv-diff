e44dfdab0cdd79f54206841ab26e5534a324c831
Vinicius Amaral <vcamaral@gmail.com>
2020-07-15 13:00:53 -0300

    Remove delimitador customizado.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-f1 = pd.read_csv('f1.csv', delimiter=';')
-f2 = pd.read_csv('f2.csv', delimiter=';')
+f1 = pd.read_csv('f1.csv')
+f2 = pd.read_csv('f2.csv')
 
 diff = f2[~f2.price.eq(f1.price)]
 diff.to_csv('diff.csv', index=False)
